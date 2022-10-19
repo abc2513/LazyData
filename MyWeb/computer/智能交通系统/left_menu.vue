8acd47881e77f3f599d512bf8dfd18497c13a7fc
abc2513 <113281468+abc2513@users.noreply.github.com>
2022-10-19 16:54:00 +0800

    Add files via upload

---
+++ MyWeb/computer/智能交通系统/left_menu.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="menu">
+        <ul>
+            <li v-for="(p,index) of li_text" :key="index" >
+                <a href={{li_href[index]}}></a>
+            </li>
+        </ul>
+	</div>
+</template>
+
+<script>
+	 export default {
+        name:"left_menu",
+		data(){
+			return {
+                li_text:['智能交通系统','2','3'],
+                li_href:['baidu.com','baidu.com','baidu.com'],
+			}
+		}
+	}
+</script>
+
+<style>
+	.menu{
+		background-color: orange;
+	}
+</style>
---
+++ MyWeb/computer/智能交通系统/main.js
@@ -0,0 +1,4 @@
+import left_menu from './left_menu.vue'
+import top_menu from '../top_menu.vue'
+Vue.component('left_menu',left_menu);
+Vue.component('top_menu',top_menu);
